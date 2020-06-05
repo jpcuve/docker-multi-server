4bb085831c51fb79cbb5a816efb2c2adc000ebc9
Jean-Pierre <jpcuvelliez@gmail.com>
2020-06-05 21:49:57 +0200

    .

--- dispatcher/api.py
+++ dispatcher/api.py
@@ -2,23 +2,13 @@ from flask import Flask, jsonify, current_app
 from flask.json import jsonify
 import os
 import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  env_vectorizers = os.environ.get('DISPATCHER_VECTORIZERS')
-  vectorizers = []
-  if env_vectorizers is not None:
-    for vectorizer in env_vectorizers.split(','):
-      res = requests.get(vectorizer)
-      if res.ok:
-        vectorizers.append(res.json())
-  env_predictors = os.environ.get('DISPATCHER_PREDICTORS')
-  predictors = []
-  if env_predictors is not None:
-    for predictor in env_predictors.split(','):
-      res = requests.get(predictor)
-      if res.ok:
-        predictors.append(res.json())
-  return jsonify(vectorizers=vectorizers, predictors=predictors)
+  vectorizers = [requests.get(vectorizer).json() for vectorizer in json.loads(os.environ.get('DISPATCHER_VECTORIZERS'))]
+  predictors = [requests.get(predictor).json() for predictor in json.loads(os.environ.get('DISPATCHER_PREDICTORS'))]
+  models = json.loads(os.environ.get('DISPATCHER_MODELS'))
+  return jsonify(vectorizers=vectorizers, predictors=predictors, models=models)
